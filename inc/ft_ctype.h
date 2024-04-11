4436eee1b99e189e0843cc2319e59a2d500ba60b
shenghongzhong <szhong@c1r4s8.42london.com>
2024-04-11 17:43:29 +0100

    changed modules, reorganised evertyhing

--- inc/ft_ctype.h
+++ inc/ft_ctype.h
@@ -1,27 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*   ft_ctype.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: szhong <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/30 19:56:12 by szhong            #+#    #+#             */
-/*   Updated: 2023/11/20 21:22:18 by szhong           ###   ########.fr       */
+/*   Created: 2024/04/11 16:55:05 by szhong            #+#    #+#             */
+/*   Updated: 2024/04/11 16:58:38 by szhong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/**
- * @brief Converts an uppercase letter to lowercase.
- *
- * @param c The character to convert.
- * @return Lowercase equivalent if `c` is an uppercase letter, 
- * 	   otherwise returns `c`.
- */
-int	ft_tolower(int c)
-{
-	if (c >= 65 && c <= 90)
-	{
-		c += 32;
-	}
-	return (c);
-}
+#ifndef FT_CTYPE_H
+#define FT_CTYPE_H
+
+int	ft_isalnum(int);
+int	ft_isalpha(int);
+int	ft_isascii(int);
+int	ft_isblank(int);
+int	ft_iscntrl(int);
+int	ft_isdigit(int);
+int	ft_islower(int);
+int	ft_isprint(int);
+int	ft_ispunct(int);
+int	ft_isspace(int);
+int	ft_isupper(int);
+int	ft_isxdigit(int);
+#endif
